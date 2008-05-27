994b7abdc16de92dae38681a41d74338a6669c7a
James <James@be3fd238-c909-4725-b312-79b88f520c4f>
2008-05-27 21:09:14 +0000

    Moving to tomcat6/java6 caused some issues with action mappings
    git-svn-id: http://192.168.1.66/svn/Java/trunk/facebook@177 be3fd238-c909-4725-b312-79b88f520c4f

--- src/com/jamescookie/facebook/MyActionMapper.java
+++ src/com/jamescookie/facebook/MyActionMapper.java
@@ -2,24 +2,22 @@ package com.jamescookie.facebook;
 
 import com.opensymphony.xwork2.config.ConfigurationManager;
 import org.apache.struts2.RequestUtils;
-import org.apache.struts2.dispatcher.mapper.ActionMapper;
 import org.apache.struts2.dispatcher.mapper.ActionMapping;
+import org.apache.struts2.dispatcher.mapper.DefaultActionMapper;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class MyActionMapper implements ActionMapper {
+public class MyActionMapper extends DefaultActionMapper {
     public ActionMapping getMapping(HttpServletRequest request, ConfigurationManager configManager) {
         ActionMapping mapping = null;
         String uri = RequestUtils.getServletPath(request);
 
         if (uri.contains("index.html")) {
             mapping = new ActionMapping("Start", "/panoramio", "", null);
+        } else {
+            mapping = super.getMapping(request, configManager);
         }
 
         return mapping;
-    }
-
-    public String getUriFromActionMapping(ActionMapping mapping) {
-        return null; // will pass to next mapper instead
     }
 }
